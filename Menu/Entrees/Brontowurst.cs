bc91cadb14e606449ff003c552c6f2648708fea2
George Widenor <eaglewidenor@ksu.edu>
2019-09-09 15:24:31 -0500

    Add Brontowurst class

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ *  Brontowurst.cs
+ *  Author: George Widenor
+ *  Description: A class made to define the object brontowurst with a price, calories, and a list of ingredients with methods to remove some ingredients from the Brontowurst.
+ */
+
+namespace Menu.Entrees
+{
+    public class Brontowurst
+    {
+        
+        private bool bun = true;
+        private bool peppers = true;
+        private bool onions = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (peppers) ingredients.Add("Peppers");
+                if (onions) ingredients.Add("Onion");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 483;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
+    }
+}
