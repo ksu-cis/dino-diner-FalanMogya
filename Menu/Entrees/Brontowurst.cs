6ab807fbaa0866e91545348a34ba5e7c8f7e04db
George Widenor <eaglewidenor@ksu.edu>
2019-09-11 14:52:02 -0500

    Add new class and comments to Brontowurst

--- Menu/Entrees/Brontowurst.cs
+++ Menu/Entrees/Brontowurst.cs
@@ -12,14 +12,22 @@ namespace Menu.Entrees
 {
     public class Brontowurst
     {
-        
+        /// <summary>
+        /// Private variable that keep track off if the bun, peppers, or onion is want to be added to the brontowurst
+        /// </summary>
         private bool bun = true;
         private bool peppers = true;
         private bool onions = true;
 
+        /// <summary>
+        /// Public variables that the user can get and set calorie total and price of the brontowurst
+        /// </summary>
         public double Price { get; set; }
         public uint Calories { get; set; }
 
+        /// <summary>
+        /// Displays the list of ingredients excluding the ingredients the customer wants held
+        /// </summary>
         public List<string> Ingredients
         {
             get
@@ -32,22 +40,34 @@ namespace Menu.Entrees
             }
         }
 
+        /// <summary>
+        /// The constructor that sets the appropriate price and calories
+        /// </summary>
         public Brontowurst()
         {
             this.Price = 5.36;
             this.Calories = 483;
         }
 
+        /// <summary>
+        /// Removes the bun from the list of ingredients
+        /// </summary>
         public void HoldBun()
         {
             this.bun = false;
         }
 
+        /// <summary>
+        /// Removes the peppers from the Brontowurst
+        /// </summary>
         public void HoldPeppers()
         {
             this.peppers = false;
         }
 
+        /// <summary>
+        /// Removes the onions from the Brontowurst
+        /// </summary>
         public void HoldOnion()
         {
             this.onions = false;
